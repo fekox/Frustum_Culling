c3e00b12b5ef50ae890879464c3a858b95f33c8a
Facundo Santos <facusantos123@gmail.com>
2022-11-19 16:46:56 -0300

    Add a funtions to set the points of the camera far plane and the points of the camera near plane

--- Assets/Scripts/FrustumCulling.cs
+++ Assets/Scripts/FrustumCulling.cs
@@ -17,14 +17,14 @@ public class FrustumCulling : MonoBehaviour
     //Plano cercano de la camara.
     [SerializeField] Vector3 nearTopLeft;
     [SerializeField] Vector3 nearTopRight;
-    [SerializeField] Vector3 nearBottomLeft;
-    [SerializeField] Vector3 nearBottomRight;
+    [SerializeField] Vector3 nearDownLeft;
+    [SerializeField] Vector3 nearDownRight;
 
     //Plano lejano de la camara.
     [SerializeField] Vector3 farTopLeft;
     [SerializeField] Vector3 farTopRight;
-    [SerializeField] Vector3 farBottomLeft;
-    [SerializeField] Vector3 farBottomRight;
+    [SerializeField] Vector3 farDownLeft;
+    [SerializeField] Vector3 farDownRight;
 
     private void Awake() //Se llama automaticamente al iniciar el scrip.
     {
@@ -58,21 +58,52 @@ public class FrustumCulling : MonoBehaviour
         farPlanePos += camera.transform.forward * camera.farClipPlane; //Actualizo el plano lejano de la camara.
         plane[1].SetNormalAndPosition(camera.transform.forward * -1, farPlanePos); //Seteo el plano lejano del frustrum.
 
-        //*Plano Izquierdo*
-        plane[2].Set3Points(camera.transform.position, farBottomLeft, farTopLeft); //Seteo el plano Izquierdo del frustrum.
+        SetFarPoints(farPlanePos);//Lamo a la funcion para setear los puntos del plano lejano del Frustrum.
+        SetNearPoints(nearPlanePos);//Lamo a la funcion para setear los puntos del plano cercano del Frustrum.
 
-        //*Plano Derecho*
-        plane[3].Set3Points(camera.transform.position, farTopRight, farBottomRight); //Seteo el Derecho lejano del frustrum.
+        //*Plano izquierdo*
+        plane[2].Set3Points(camera.transform.position, farDownLeft, farTopLeft); //Seteo el plano Izquierdo del frustrum.
+
+        //*Plano derecho*
+        plane[3].Set3Points(camera.transform.position, farTopRight, farDownRight); //Seteo el Derecho lejano del frustrum.
 
         //*Plano de arriba*
         plane[4].Set3Points(camera.transform.position, farTopLeft, farTopRight); //Seteo el plano de arriba del frustrum.
 
         //*Plano de abajo*
-        plane[5].Set3Points(camera.transform.position, farBottomRight, farBottomLeft); //Seteo el plano de abajo del frustrum.
+        plane[5].Set3Points(camera.transform.position, farDownRight, farDownLeft); //Seteo el plano de abajo del frustrum.
 
         for (int i = 2; i < maxPlanes; i++)
         {
             plane[i].Flip();//Hace que el plano mire en la direccion opuesta.
         }
+    }
+
+    //Funcion para setear los puntos del plano lejano
+    public void SetFarPoints(Vector3 farPos)
+    {
+        Vector3 farPlaneDist = camera.transform.position + (camera.transform.forward * camera.farClipPlane); //Guardo la distancia del plano lejano de la camara.
+
+        farTopLeft = farPlaneDist + (camera.transform.up) - (camera.transform.right); //Seteo el punto de arriba a la izquierda.
+
+        farTopRight = farPlaneDist + (camera.transform.up) + (camera.transform.right); //Seteo el punto de arriba a la derecha.
+
+        farDownLeft = farPlaneDist - (camera.transform.up) - (camera.transform.right); //Seteo el punto de abajo a la izquierda.
+
+        farDownRight = farPlaneDist - (camera.transform.up) + (camera.transform.right); //Seteo el punto de abajo a la derecha.
+    }
+
+    //Funcion para setear los puntos del plano cercano
+    public void SetNearPoints(Vector3 nearPos)
+    {
+        Vector3 nearPlaneDist = camera.transform.position + (camera.transform.forward * camera.nearClipPlane); //Guardo la distancia del plano cercano de la camara.
+
+        nearTopLeft = nearPlaneDist + (camera.transform.up) - (camera.transform.right); //Seteo el punto de arriba a la izquierda.
+
+        nearTopRight = nearPlaneDist + (camera.transform.up) + (camera.transform.right); //Seteo el punto de arriba a la derecha.
+
+        nearDownLeft = nearPlaneDist - (camera.transform.up) - (camera.transform.right); //Seteo el punto de abajo a la izquierda.
+
+        nearDownRight = nearPlaneDist - (camera.transform.up) + (camera.transform.right); //Seteo el punto de abajo a la derecha.
     }
 }
